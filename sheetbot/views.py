387990e996701c1fc2c843e62341f7f7064acd7a
Hellwish <a.yush11@yahoo.com>
2018-01-11 14:38:06 +0530

    views change

--- sheetbot/views.py
+++ sheetbot/views.py
@@ -44,6 +44,7 @@ def post_fb_msg(fbid,received_msg):
 	user_details_url = "https://graph.facebook.com/v2.6/%s"%fbid
 	user_details_params = {'fields':'first_name,last_name,profile_pic', 'access_token':PAGE_ACCESS_TOKEN}
 	user_details = requests.get(user_details_url, user_details_params).json()
+	pprint(user_details)
 	tokens=re.sub(r"[a-zA-Z0-9\s]",' ',received_msg).lower().split()
 	for token in tokens:
 		 list1=['hy','hello','sup','hola','hey']
