7fc36efd22df2409e5148922f870f62a3bf9576c
Hellwish <a.yush11@yahoo.com>
2018-01-11 13:39:04 +0530

    change urls.py(spreadsheet)

--- spreadsheet/urls.py
+++ spreadsheet/urls.py
@@ -15,8 +15,9 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path
+from django.conf.urls import include,url
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path(r'^sheetbot/',include('sheetbot.urls'))
+    url(r'^sheetbot/',include('sheetbot.urls'))
 ]
